689b2982cf239c7d53fb5588503e0f0c3788ae29
ain-limon <ainlimon0@gmail.com>
2025-04-15 21:37:21 +0600

    base

--- src/LibraryMembershipSystem.java
+++ src/LibraryMembershipSystem.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class LibraryMembershipSystem {
+
+    private static final String DATA_FILE = "members.csv";
+
+
+        while (true) {
+            System.out.println("\n--- Library Membership Management System ---");
+            System.out.println("1. Register New Member");
+            System.out.println("2. View Member Information");
+            System.out.println("3. Update Member Information");
+            System.out.println("4. Delete Member");
+            System.out.println("5. Display All Members");
+            System.out.println("6. Find Member by Name");
+            System.out.println("7. Save Data");
+            System.out.println("8. Generate Report");
+            System.out.println("9. Borrow Book");
+            System.out.println("10. Exit");
+            System.out.print("Enter your choice: ");
+
